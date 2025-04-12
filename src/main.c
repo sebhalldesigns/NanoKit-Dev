9000b85c0d86c098c7216480ef4bd36f0af736ab
sebhalldesigns <sebhalldesigns@gmail.com>
2025-04-12 23:14:20 +0100

    added dockpanel test

--- src/main.c
+++ src/main.c
@@ -3,17 +3,56 @@
 #include <Window.xml.h>
 #include <SecondWindow.xml.h>
 
+#include <stdlib.h>
+
 #include "../NanoKit/lib/kit/NanoKit.h"
 
-void AppLaunched(void *app)
+void AppLaunched()
 {
     printf("App launched\n");
+
 }
 
 int main()
 {   
     nkWindow* window = Window_Create();
-    nkWindow* secondWindow = SecondWindow_Create();
+
+    nkDockPanel* dockPanel = nkDockPanel_Create();
+    window->RootView = dockPanel->View;
+
+    dockPanel->View->BackgroundColor = COLOR_MAGENTA;
+
+    /* create many dockpanel views */
+    for (int i = 0; i < 5000; i++)
+    {
+        nkView* view = CreateView();
+
+        nkColor randomColors[] = {COLOR_RED, COLOR_GREEN, COLOR_BLUE, COLOR_YELLOW, COLOR_CYAN};
+        view->BackgroundColor = randomColors[rand() % 5];
+
+        view->SizeRequest = (nkSize){0.1, 0.1};
+        
+        switch (i % 4)
+        {
+            case 0:
+                view->DockPosition = DOCK_POSITION_TOP;
+                break;
+            case 1:
+                view->DockPosition = DOCK_POSITION_BOTTOM;
+                break;
+            case 2:
+                view->DockPosition = DOCK_POSITION_LEFT;
+                break;
+            case 3:
+                view->DockPosition = DOCK_POSITION_RIGHT;
+                break;
+        }
+
+
+        AddChildView(dockPanel->View, view);
+    }
+
+    //nkWindow* secondWindow = SecondWindow_Create();
 
     printf("Hello World\n"); 
 
