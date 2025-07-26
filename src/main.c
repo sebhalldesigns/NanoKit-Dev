34a0925bb481a9a3c78100de57fdf1316ed55823
sebhall <sebhalldesigns@gmail.com>
2025-07-26 23:30:04 +0100

    working on web parallel build

--- src/main.c
+++ src/main.c
@@ -12,7 +12,10 @@ static nkFont_t font;
 float pointerX = 0.0f;
 float pointerY = 0.0f;
 
+#if _WIN32
 static LARGE_INTEGER start, end, lastStart, frequency = {0};
+#endif 
+
 static float fps = 0.0f;
 
 static char fpsText[64];
@@ -25,21 +28,31 @@ void PointerMoveCallback(nkWindow_t *window, float x, float y)
     pointerX = x;
     pointerY = y;
 
-    //nkWindow_RequestRedraw(window);
+    nkWindow_RequestRedraw(window);
 }   
 
 void WindowResizeCallback(nkWindow_t *window, float width, float height)
 {
-    //printf("Window '%s' resized to %.2f x %.2f\n", window->Title, width, height);
+    //printf("Window '%s' resized to %.2f x %.2f\n", window->title, width, height);
 
-    QueryPerformanceCounter(&start);
+    //QueryPerformanceCounter(&start);
+
+     #if __EMSCRIPTEN__
+        double startTime = emscripten_get_now();
+    #endif
 
     nkWindow_LayoutViews(window); // Layout the views in the window after resizing
 
-    QueryPerformanceCounter(&end);
+    //QueryPerformanceCounter(&end);
 
-    double elapsedMs = (double)(end.QuadPart - start.QuadPart) * 1000.0 / frequency.QuadPart;
-    printf("Layout took %.3f ms\n", elapsedMs);
+    //double elapsedMs = (double)(end.QuadPart - start.QuadPart) * 1000.0 / frequency.QuadPart;
+    //printf("Layout took %.3f ms\n", elapsedMs);
+
+     #if __EMSCRIPTEN__
+        double endTime = emscripten_get_now();
+        double elapsedMs = endTime - startTime;
+        printf("Layout took %.2f ms\n", elapsedMs);
+    #endif
 }
 
 void WindowCodepointInputCallback(nkWindow_t *window, uint32_t codepoint)
@@ -81,14 +94,20 @@ void WindowDrawCallback(nkWindow_t *window)
     char title[256];
     snprintf(title, sizeof(title), "(%.1f, %.1f)", pointerX, pointerY);
 
-    QueryPerformanceCounter(&start);
+    #if _WIN32
+        QueryPerformanceCounter(&start);
+    #endif
+
+    #if __EMSCRIPTEN__
+        double startTime = emscripten_get_now();
+    #endif
 
     nkDraw_Begin(&window->drawContext, window->width, window->height);
 
-    nkWindow_RedrawViews(window); // Redraw the views in the window
+    //nkWindow_RedrawViews(window); // Redraw the views in the window
 
 
-    #if 0
+    
     nkDraw_SetColor(&window->drawContext, (nkVector4_t){0.0f, 0.0f, 0.0f, 1.0f});
 
     float width = 100.0f;
@@ -110,48 +129,64 @@ void WindowDrawCallback(nkWindow_t *window)
             renderCount++;
         }
     }
-    #endif
+  
+
+    nkDraw_SetColor(&window->drawContext, NK_COLOR_WHITE); // Orange
+    nkDraw_Rect(&window->drawContext, 0.0f, pointerY - 2.0f, window->width, 4.0f);
+    nkDraw_Rect(&window->drawContext, pointerX - 2.0f, 0.0f, 4.0f, window->height);
 
     nkDraw_End(&window->drawContext);
 
-    QueryPerformanceCounter(&end);
+    #if _WIN32
+        QueryPerformanceCounter(&end);
 
-    double elapsedMs = (double)(end.QuadPart - start.QuadPart) * 1000.0 / frequency.QuadPart;
-    printf("Drawing took %.2f ms\n", elapsedMs);
+        double elapsedMs = (double)(end.QuadPart - start.QuadPart) * 1000.0 / frequency.QuadPart;
+        printf("Drawing took %.2f ms\n", elapsedMs);
 
-    /* calculate FPS */
-    if (lastStart.QuadPart == 0)
-    {
-        lastStart = start;
-    }
+        /* calculate FPS */
+        if (lastStart.QuadPart == 0)
+        {
+            lastStart = start;
+        }
 
-    double elapsedTime = (double)(start.QuadPart - lastStart.QuadPart) * 1000.0 / frequency.QuadPart; // in milliseconds
-    if (elapsedTime > 0.0)
-    {
-        fps = 1000.0 / elapsedTime; // Calculate FPS
-    }
+        double elapsedTime = (double)(start.QuadPart - lastStart.QuadPart) * 1000.0 / frequency.QuadPart; // in milliseconds
+        if (elapsedTime > 0.0)
+        {
+            fps = 1000.0 / elapsedTime; // Calculate FPS
+        }
 
-    lastStart = start; // Update lastStart for the next frame
+        lastStart = start; // Update lastStart for the next frame
 
-    static double timeAccumulator = 0.0;
-    timeAccumulator += elapsedTime;
+        static double timeAccumulator = 0.0;
+        timeAccumulator += elapsedTime;
+        
 
-    if (timeAccumulator >= 200.0) // Update title at 5 Hz (200 ms intervals)
-    {
-        snprintf(fpsText, sizeof(fpsText), "My Window FPS: %.2f", fps);
+        if (timeAccumulator >= 200.0) // Update title at 5 Hz (200 ms intervals)
+        {
+            snprintf(fpsText, sizeof(fpsText), "My Window FPS: %.2f", fps);
 
-        nkWindow_SetTitle(window, fpsText);
-        timeAccumulator = 0.0; // Reset accumulator
-    }
+            nkWindow_SetTitle(window, fpsText);
+            timeAccumulator = 0.0; // Reset accumulator
+        }
 
+    #endif
+
+    #if __EMSCRIPTEN__
+        double endTime = emscripten_get_now();
+        double elapsedMs = endTime - startTime;
+        printf("Drawing took %.2f ms\n", elapsedMs);
+    #endif
 
 }   
 
 nkWindow_t window = {0};
 
-nkView_t views[10000] = {0}; // Array to hold views
+nkView_t views[1000] = {0}; // Array to hold views
+
+nkDockView_t dockView  = {0};
 
 
+/* IMPORTANT: ON WEB, MAKE SURE NOTHING IS LOCAL TO THE MAIN FUNCTION AS IT IS TEMPORARY */
 //DWORD WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 int main()
 {
@@ -165,7 +200,9 @@ int main()
     window.pointerMoveCallback = (nkWindowPointerMoveCallback_t)PointerMoveCallback;
     nkWindow_Create(&window, "My Window", 800, 600);
 
-    nkDockView_t dockView  = {0};
+    window.backgroundColor = NK_COLOR_GREEN;
+
+    
     nkDockView_Create(&dockView);
 
     window.rootView = (nkView_t *)&dockView.view; // Set the root view to the dock view
@@ -173,7 +210,7 @@ int main()
     dockView.view.backgroundColor = NK_COLOR_LIGHT_GRAY; // Set the background color of the dock view
 
      /* create many dockpanel views */
-    for (int i = 0; i < 10000; i++)
+    for (int i = 0; i < 1000; i++)
     {
         nkView_t* view = &views[i];
         nkView_Create(view, "DockPanel View");
@@ -182,6 +219,7 @@ int main()
         view->backgroundColor = randomColors[rand() % 5];
 
         view->sizeRequest = (nkSize_t){0.1, 0.1};
+        view->arrangeCallback = NULL;
         
         switch (i % 4)
         {
@@ -207,8 +245,9 @@ int main()
     //nkFont_Load(&font, "build/Roboto-Regular.ttf", 16.0f, atlas_buffer, 512, 512);
     nkFont_LoadFromMemory(&font, NKFonts_fonts_Roboto_Regular_ttf, NKFonts_fonts_Roboto_Regular_ttf_size, 16.0f, atlas_buffer, 512, 512);
 
-    QueryPerformanceFrequency(&frequency);
-
+    #if _WIN32
+        QueryPerformanceFrequency(&frequency);
+    #endif
 
     while (nkWindow_PollEvents())
     {
@@ -216,5 +255,4 @@ int main()
     }
 
     return 0;
-
 }
